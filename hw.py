ff95824a5f813d8ae8677789311b926bcf32d8d8
Shaurya094 <Shauryauprari094@gmail.com>
2025-09-20 14:51:04 +1000

    hw

--- hw.py
+++ hw.py
@@ -2,9 +2,9 @@ n = input("What is your name?\n")
 x = input(f"Hello, {n}! How is your day?\n")
 x = x.lower()
 x = str(x)
-if x == "good" or "great" "amazing":
+if x == "good" or "great" or "amazing":
     print(f"That's great, {n}!")
-elif x == "bad" or "horrible" or "not good":
+elif x == "bad" or "horrible":
     print (f"Sorry to hear that, {n}, hope it gets better!")
 else:
     print ("I'm not smart enough for that!")
